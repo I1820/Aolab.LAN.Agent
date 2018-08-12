0df4f6f629d66eba67bdaf386966024cab828dc7
Parham Alvani <parham.alvani@gmail.com>
2018-08-12 16:18:08 +0430

    Adds I1820 specific errors.

---
+++ I1820/errors.py
@@ -0,0 +1,12 @@
+# In The Name of God
+# =======================================
+# [] File Name : errors.py
+#
+# [] Creation Date : 12-08-2018
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+
+class I1820Exception(Exception):
+    pass
