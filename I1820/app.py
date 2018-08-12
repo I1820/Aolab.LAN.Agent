4244fbc297ce9a18fd92cc6af1c82761bce8e6ee
Parham Alvani <parham.alvani@gmail.com>
2018-08-12 16:18:27 +0430

    Changes structure of I1820 to our new architecture.

--- I1820/app.py
+++ I1820/app.py
@@ -8,34 +8,38 @@
 # =======================================
 from .domain.log import I1820Log
 from .domain.notif import I1820Notification
-from .domain.agent import I1820Agent
-from . import i1820_id
+from .errors import I1820Exception
 
 import paho.mqtt.client as mqtt
 import logging
 import asyncio
 import threading
+import re
+import base64
+import json
 
 i1820_logger_app = logging.getLogger('I1820.app')
 
 
 class I1820App:
-    def __init__(self, tenant_id: str, mqtt_ip: str, mqtt_port: int=1883,
-                 logger=None):
+    def __init__(self, dev_eui: str, token: str, mqtt_ip: str,
+                 mqtt_port: int=1883, logger=None):
         # MQTT Up and Running
         self.client = mqtt.Client()
         self.client.connect(mqtt_ip, mqtt_port)
         self.client.on_connect = self._on_connect
 
-        # Tenant Identification
-        self.tenant_id = tenant_id
+        # Device Identification
+        p = re.compile(r'[a-fA-f0-9]{16}')
+        if p.fullmatch(dev_eui) is None:
+            raise I1820Exception("Invalid DevEUI")
+        self.dev_eui = dev_eui
 
-        # I1829 Agent
-        self.agent = I1820Agent(str(i1820_id), [])
+        # Token
+        self.token = token
 
         # Notification/Action handlers
         self.notification_handlers = {}
-        self.action_handlers = {}
 
         # Event loop
         self.loop = asyncio.new_event_loop()
@@ -43,18 +47,14 @@ class I1820App:
         if logger is None:
             self.logger = i1820_logger_app
 
-    def add_thing(self, type, id):
-        self.agent.things.append({'type': type, 'id': id})
-
     def run(self):
-        print(" * Node ID: %s" % i1820_id)
+        print(" * Node ID: %s" % self.dev_eui)
         t = threading.Thread(target=self._run)
         t.daemon = True
         t.start()
 
     def _run(self):
         asyncio.set_event_loop(self.loop)
-        self._ping()
         self._loop()
         try:
             self.loop.run_forever()
@@ -77,35 +77,31 @@ class I1820App:
         return _action
 
     def log(self, type, device, states):
-        log = I1820Log(type, device, states, str(i1820_id))
-        self.client.publish('I1820/%s/log/send' % self.tenant_id,
-                            log.to_json())
-
-    def _ping(self):
-        self.client.publish('I1820/%s/agent/ping' % self.tenant_id,
-                            self.agent.to_json())
-        self.loop.call_later(10, self._ping)
+        log = I1820Log(type, device, states)
+        data = {
+            'data': base64.b64encode(
+                log.to_json().encode('ascii')).decode('ascii'),
+            'token': self.token,
+        }
+        self.logger.info('log: %s' % data)
+        self.client.publish('log/%s/send' % self.dev_eui,
+                            json.dumps(data))
 
     def _loop(self):
         self.client.loop()
         self.loop.call_soon(self._loop)
 
     def _on_connect(self, client, userdata, flags, rc):
-        client.subscribe('I1820/%s/configuration/request' % self.tenant_id)
-        client.message_callback_add('I1820/%s/configuration/request' %
-                                    self.tenant_id, self._on_notification)
+        self.logger.info("MQTT connection is here")
+        client.subscribe('notification/%s/request' % self.dev_eui)
+        client.message_callback_add('notification/%s/request' %
+                                    self.dev_eui, self._on_notification)
 
     def _on_notification(self, client, userdata, message):
         notif = I1820Notification.from_json(message.payload.decode('ascii'))
 
-        if notif.agent != str(i1820_id):
-            return
-
         try:
-            ret = self.notification_handlers[notif.type](notif)
-            if ret is True:
-                client.publish('I1820/%s/configuration/change'
-                               % self.tenant_id, notif.to_json())
+            self.notification_handlers[notif.type](notif)
             self.logger.info('device: %s -- settings: %r' %
                              (notif.device, notif.settings))
         except KeyError:
