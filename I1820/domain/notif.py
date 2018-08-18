e9e17eb9259214158b3ad480f4134028f34871d9
Parham Alvani <parham.alvani@gmail.com>
2018-08-19 01:47:43 +0430

    Better notification structure.

--- I1820/domain/notif.py
+++ I1820/domain/notif.py
@@ -19,29 +19,18 @@ class I1820Notification:
     :type device: str
     :param settings: configurations wanted to apply on target end device.
     :type settings: dict
-    :param agent: identification of target end device agent [Raspberry PI].
-    :type agent: str
     '''
-    def __init__(self, type: str, device: str,
-                 settings: list,
-                 agent: str):
-
-        for setting in settings:
-            if 'name' not in setting or 'value' not in setting:
-                raise ValueError(
-                    'settings must be an array of names and values.')
-
-        self.type = type
+    def __init__(self, kind: str, device: str,
+                 settings: list):
+        self.type = kind
         self.device = device
         self.settings = settings
-        self.agent = agent
 
     def to_json(self):
             result = {
-                'type': self.type,
+                'kind': self.kind,
                 'device': self.device,
                 'settings': self.settings,
-                'agent': self.agent
             }
             return json.dumps(result)
 
@@ -49,8 +38,7 @@ class I1820Notification:
     def from_json(cls, raw):
         raw_values = json.loads(raw)
 
-        type = raw_values['type']
+        kind = raw_values['kind']
         device = raw_values['device']
         settings = raw_values['settings']
-        agent = raw_values['agent']
-        return cls(type, device, settings, agent)
+        return cls(kind, device, settings)
